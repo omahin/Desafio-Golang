f86f11afee164dcfb3e97b0d9712c52ae824da9d
JonissonGomes <joni_gomes@hotmail.com>
2022-02-21 21:03:51 -0300

    Adicionando corpo inicial

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	// mensagem := ""
+	nomeComprador := ""
+	quantidadeBilhetes := 0.0
+
+	fmt.Printf("Qual o seu nome ? ")
+	fmt.Scanln(&nomeComprador)
+
+	fmt.Printf("Quantos bilhetes você deseja comprar: ")
+	fmt.Scanln(&quantidadeBilhetes)
 
 }
