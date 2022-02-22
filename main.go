7410164e02eabf788e1b9363b0b5b238e081782f
JonissonGomes <joni_gomes@hotmail.com>
2022-02-21 21:12:30 -0300

    Adicionando validação para os bilhetes

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	// mensagem := ""
-	nomeComprador := ""
-	quantidadeBilhetes := 0.0
 
+	// Variáveis
+	nomeComprador := ""
+	var quantidadeBilhetes uint = 0
+
+	// Pergunta 1
 	fmt.Printf("Qual o seu nome ? ")
 	fmt.Scanln(&nomeComprador)
 
-	fmt.Printf("Quantos bilhetes você deseja comprar: ")
+	// Pergunta 2
+	fmt.Printf("Quantos bilhetes você deseja comprar? ")
 	fmt.Scanln(&quantidadeBilhetes)
+
+	// Validação
+	if quantidadeBilhetes > 1 {
+		fmt.Println(nomeComprador, ", você deseja confirmar a compra de ", quantidadeBilhetes, " bilhetes?")
+	} else if quantidadeBilhetes == 1 {
+		fmt.Println(nomeComprador, ", você deseja confirmar a compra de ", quantidadeBilhetes, " bilhete?")
+	} else {
+		fmt.Println("Informe um valor válido(ex: 1)")
+	}
 
 }
